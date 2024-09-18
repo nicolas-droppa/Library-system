45d5397c6fc99ddbc6a7febbec1479e03425ff99
nicolas-droppa <droppa.nicolas@gmail.com>
2024-09-18 23:27:31 +0200

    Update LibrarySimulation.cpp

--- LibrarySimulation.cpp
+++ LibrarySimulation.cpp
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define lineLength		100
+#define indentation		3
 
 void showMenu() {
 	printf("+---------------------------+\n");
@@ -13,179 +14,35 @@ void showMenu() {
 	printf("+---------------------------+\n");
 }
 
-char getChoice() {
-	char choice;
-	
-	while (1) {
-		choice = getch();
-		switch (choice) {
-			case '1':
-			case '2':
-			case '3':
-				return choice;
-			default:
-				continue;
-		}
-	}
+FILE *createDatabase(FILE *bookDatabase) {
+	printf("Database does not exist... Creating new database!\n");
+	bookDatabase = fopen("bookDatabase.txt", "w");
+	fprintf(bookDatabase, "{\n%*s\"books\":\n%*s[\n\n%*s],""\n%*s\"users\":\n%*s[\n\n%*s]\n}\n\n"
+					   , indentation, "", indentation*2, "", indentation*2, "", indentation, ""
+					   , indentation*2, "", indentation*2, "");
+	fclose(bookDatabase);
+	return bookDatabase;
 }
 
-int alreadyInDatabase(char **database, char *title, int *dbLineCount) {
-	char line[lineLength];
-	int shiftString = 1;
-	
-	for (int i = 0; i < (*dbLineCount); i++) {
-		for (int j = 0; j < strlen(database[i]); j++) {
-			if(database[i][j] == ' ')
-				break;
-			shiftString++;
-		}
-		
-        if (strcmp(database[i] + shiftString, title) == 0)
-        	return 1;
-        	
-        shiftString = 1;
-    }
+char **loadDatabase(int *dbLineCount) {
+	FILE *bookDatabase;
 
-    return 0;
-}
-
-int countDigits(int number) {
-    int count = 0;
-
-    if (number == 0) {
-        return 1;
-    }
-
-    while (number != 0) {
-        number = number / 10;
-        ++count;
-    }
-
-    return count;
-}
-
-void AddBookToDB(char **database, int *dbLineCount) {
-	char title[lineLength];
-	unsigned int errorState = 0;
-
-	while(1) {
-		system("cls");
-		printf("+---AddBook----------+\n\n");
-		printf("Write EXIT! to return\n\n");
-		printf("+--------------------+\n");
-		
-		if (errorState) {
-			printf("[ERROR] Book is already in database!\n\n");
-			errorState = 0;
-		}
-		
-		printf("Enter Title: ");
-		fgets(title, sizeof(title), stdin);
-		
-		if (strcmp(title, "EXIT!\n") == 0) {
-			return;
-		}
-		
-		if (alreadyInDatabase(database, title, dbLineCount)) {
-			errorState = 1;
-			continue;
-		} else {
-			for (int i = 0; i < (*dbLineCount); i++) {
-				if (strcmp(database[i], "\n") == 0) {
-					snprintf(database[i], sizeof(title) + i+2, "%d %s", i+1, title);
-					return;
-				}
-    		}
-    		
-    		database = (char **)realloc(database, ((*dbLineCount) + 1) * sizeof(char *));
-	        database[(*dbLineCount)] = (char *)malloc((strlen(title) + countDigits((*dbLineCount))+1) * sizeof(char));
-	        snprintf(database[(*dbLineCount)], sizeof(title) + countDigits((*dbLineCount))+1, "%d %s", (*dbLineCount)+1, title);
-	        (*dbLineCount)++;
-	        return;
-		}	
-	}
-}
-
-void RemoveBookFromDB(char **database, int *dbLineCount) {
-	char title[lineLength];
-	unsigned int errorState = 0;
-	
-	while(1) {
-		system("cls");
-		printf("+---RemoveBook-------+\n\n");
-		printf("Write EXIT! to return\n\n");
-		printf("+--------------------+\n");
-		
-		if (errorState) {
-			printf("[ERROR] Book is not in database!\n\n");
-			errorState = 0;
-		}
-		
-		printf("Enter Title: ");
-		fgets(title, sizeof(title), stdin);
-		
-		if (strcmp(title, "EXIT!\n") == 0) {
-			return;
-		}
-		
-		if (!alreadyInDatabase(database, title, dbLineCount)) {
-			errorState = 1;
-			continue;
-		} else {
-			for (int i = 0; i < (*dbLineCount); i++) {				
-				int shiftString = 1;
-				
-				for (int j = 0; j < strlen(database[i]); j++) {
-					if(database[i][j] == ' ')
-						break;
-					shiftString++;
-				}
-				
-		        if (strcmp(database[i] + shiftString, title) == 0) {
-		        	database[i] = (char *)realloc(database[i], (1) * sizeof(char ));
-					strcpy(database[i], "\n");
-					return;
-				}
-    		}
-		}	
-	}
-}
-
-int choiceHandler(char choice, char **database, int *dbLineCount) {
-	switch(choice) {
-		case '1':
-			AddBookToDB(database, dbLineCount);
-		break;
-		case '2':
-			RemoveBookFromDB(database, dbLineCount);
-		break;
-		case '3':
-			return 0;
-		break;
-		default:
-			return 1;
-	}
-}
-
-char **loadDatabase(int *dbLineCount) {	
-	FILE *bookDatabase;	
-	
-	if((bookDatabase = fopen("bookDatabase.txt", "r")) == NULL) {	
-		printf("Database does not exist... Creating new database!\n");	
-		bookDatabase = fopen("bookDatabase.txt", "w");	
-		fclose(bookDatabase);
+	if((bookDatabase = fopen("bookDatabase.txt", "r")) == NULL) {
+		createDatabase(bookDatabase);
 		bookDatabase = fopen("bookDatabase.txt", "r");
-	}	
-		
+	}
+
 	char line[lineLength];
-	
+
 	int lineCounter = 0;
 	char **database = (char **)malloc(1 * sizeof(char *));
-	
+
 	while (fgets(line, sizeof(line), bookDatabase) != NULL) {
         database = (char **)realloc(database, (lineCounter + 1) * sizeof(char *));
         database[lineCounter] = (char *)malloc((strlen(line) + 1) * sizeof(char));
         strcpy(database[lineCounter], line);
+		//replace \0 for \n in line
+		printf("nehe", database[lineCounter]);
         lineCounter++;
     }
 
@@ -195,38 +52,65 @@ char **loadDatabase(int *dbLineCount) {
 }
 
 void saveDatabase(char **database, int dbLineCount) {
-	FILE *bookDatabase;	
-	
-	if((bookDatabase = fopen("bookDatabase.txt", "w")) == NULL) {	
-		printf("An error has occured while opening database... Ending program!\n");	
+	FILE *bookDatabase;
+
+	if((bookDatabase = fopen("bookDatabase.txt", "w")) == NULL) {
+		printf("An error has occured while opening database... Ending program!\n");
 		exit(0);
 	}
-	
+
 	for (int i = 0; i < dbLineCount; i++) {
 		fputs(database[i], bookDatabase);
         free(database[i]);
     }
-    
+
     free(database);
     fclose(bookDatabase);
 }
 
 void printDatabase(char **database, int dbLineCount) {
 	for (int i = 0; i < dbLineCount; i++) {
-		if(!strcmp(database[i], "\n") == 0) // skip empty slot
-			printf("%s", database[i]);
+		printf("%s", database[i]);
     }
+}
+
+int addBook(char **database, int dbLineCount) {
+
+}
+
+char getChoice() {
+	char choice;
+	int errorState = 0;
+
+	while (1) {
+		if (errorState == 1) {
+			printf("Error\n");
+		}
+		showMenu();
+		printf("Please enter an option: ");
+
+		scanf("%c", &choice);
+
+		switch(choice) {
+			case '1':
+				exit(1);
+			case '2':
+				exit(2);
+			case '3':
+				exit(0);
+			default:
+				errorState = 1;
+		}
+	}
 }
 
 int main() {
 	int dbLineCount = 0;
 	char **database = loadDatabase(&dbLineCount);
-	
+
 	while(1) {
 		printDatabase(database, dbLineCount);
-		showMenu();
-		if(choiceHandler(getChoice(), database, &dbLineCount) == 0)
-			break;
+		getChoice();
 	}
 	saveDatabase(database, dbLineCount);
 }

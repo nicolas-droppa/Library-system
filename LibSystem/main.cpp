e9548e2195c6b1e14e24c0775815e504c223a46d
nicolas-droppa <droppa.nicolas@gmail.com>
2024-09-23 23:59:09 +0200

    Update main.cpp

--- LibSystem/main.cpp
+++ LibSystem/main.cpp
@@ -47,8 +47,34 @@ void addBorrowedBook(User& user, const string& title, const string& hasFrom, con
     user.borrowedBooks.push_back(borrowed);
 }
 
-int main() {
+void printDatabase(const BookDatabase& bookDb, const UserDatabase& userDb) {
+    std::cout << "Books in the database:\n";
+    for (const auto& book : bookDb.books) {
+        std::cout << "Title: " << book.title << ", Author: " << book.author
+                  << ", Release: " << book.release << ", Available: " << book.available << "\n";
+    }
 
+    std::cout << "\nUsers in the database:\n";
+    for (const auto& user : userDb.users) {
+        std::cout << "Name: " << user.name << ", Phone: " << user.phone << ", Email: " << user.email << "\n";
+        for (const auto& borrowed : user.borrowedBooks) {
+            std::cout << "  Borrowed: " << borrowed.title << ", From: " << borrowed.hasFrom
+                      << ", Return: " << borrowed.returnDate << "\n";
+        }
+    }
+}
+
+int main() {
+    BookDatabase bookDb;
+    UserDatabase userDb;
+
+    addBook(bookDb, "Kniha 1", "Janko Hrasko", "2005", true);
+    addBook(bookDb, "Kniha 2", "Janko Hrasko", "2018", false);
+
+    addUser(userDb, "John Doe", "0905 105 615", "j.doe@gmail.com");
+    addBorrowedBook(userDb.users[0], "Kniha 1", "1-9-2024", "18-9-2024");
+    
+    printDatabase(bookDb, userDb);
 
     return 0;
 }
